25aaa7cab3f040d2cca43bfc36e8bc09bff112e4
aparna <aparna@bio-fa02son201x.local>
2019-06-04 17:05:15 -0500

    finalized code skeleton

---
+++ Acme.py
@@ -0,0 +1,25 @@
+
+
+class AcmeData(object):
+
+    def __init__(self):
+        pass
+
+    def give_users_org_names(self):
+        pass
+
+    def replace_org_ids(self):
+        pass
+
+    def give_users_org_ids(self):
+        pass
+
+    def separate_visitors(self):
+        pass
+
+    def give_tickets_org_ids(self):
+        pass
+
+
+
+
---
+++ Dataset.py
@@ -0,0 +1,28 @@
+class Dataset(object):
+
+    def __init__(self):
+        pass
+
+    def open_file(self):
+        pass
+
+    def clean_data(self):
+        pass
+
+    def load_to_pandas(self):
+        pass
+
+    def get_dataframe(self):
+        pass
+
+    def replace_dataframe(self):
+        pass
+
+    def create_hundred_entity_chunks(self):
+        pass
+
+    def to_json(self):
+        pass
+
+    def upload(self):
+        pass
