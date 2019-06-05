26b3563cda310da00ebff94b15ba8b20bd8feaf0
aparna <aparna@bio-fa02son201x.local>
2019-06-04 20:30:54 -0500

    delete extra copy of file

--- test_dataset.py
+++ test_dataset.py
@@ -1,12 +1,19 @@
-from unittest import TestCase
+from unittest import *
 
 
 class TestDataset(TestCase):
-    def test_open_file(self):
+    def test_open_file_base_case(self):
         self.fail()
 
-    def test_clean_data(self):
+    def if_file_not_found_throw_exception(self):
         self.fail()
+
+    def test_clean_data_base(self):
+        self.fail()
+
+    def test_clean_data_for_emails(self):
+        self.fail()
+
 
     def test_load_to_pandas(self):
         self.fail()
