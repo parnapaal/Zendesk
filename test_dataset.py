b3928618227d3970759fef42fe701dfebe368db2
aparna <aparna@bio-fa02son201x.local>
2019-06-04 20:00:11 -0500

    zipped up previous work -- added an __init__.py

---
+++ test_dataset.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestDataset(TestCase):
+    def test_open_file(self):
+        self.fail()
+
+    def test_clean_data(self):
+        self.fail()
+
+    def test_load_to_pandas(self):
+        self.fail()
+
+    def test_get_dataframe(self):
+        self.fail()
+
+    def test_replace_dataframe(self):
+        self.fail()
+
+    def test_create_hundred_entity_chunks(self):
+        self.fail()
+
+    def test_to_json(self):
+        self.fail()
+
+    def test_upload(self):
+        self.fail()
